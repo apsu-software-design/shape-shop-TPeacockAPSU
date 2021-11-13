efc5774b707516c66214465754f3a050cfb6c93b
TPeacockAPSU <79169949+TPeacockAPSU@users.noreply.github.com>
2021-11-13 13:16:45 -0600

    Add files via upload

---
+++ src/productView.ts
@@ -0,0 +1,44 @@
+﻿import { Product } from './products';
+import { ShopModel } from './shopModel';
+
+
+export class ProductView {
+
+    products: Product[];
+    quantities: number[];
+
+    constructor(shopModel: ShopModel) {
+        this.products = shopModel.getProducts();
+        this.quantities = shopModel.getQuantities();
+    }
+
+    public getNameView(): string {
+        let viewString: string = "";
+
+        for (let i = 0; i < this.products.length; i++) {
+            viewString += i + ": " + this.products[i].getName() + "\n";
+        }
+        return viewString;
+    }
+
+    public getPriceView(): string {
+        let total: number = 0;
+        for (let i = 0; i < this.products.length; i++) {
+            total += this.products[i].getPrice() * this.quantities[i];
+        }
+        return "Total of Shopping Cart: " + total;
+    }
+
+    public getView(): string {
+        let viewString: string = "";
+        for (let i = 0; i < this.products.length; i++) {
+            viewString += "\n";
+            viewString += "       Name: " + this.products[i].getName() + "\n";
+            viewString += "      Price: " + this.products[i].getPrice() + "\n";
+            viewString += "Description: " + this.products[i].getDescription() + "\n";
+            viewString += "   Quantity: " + this.quantities[i] + "\n";
+        }
+        return viewString;
+    }
+
+}
--- src/ui.ts
+++ src/ui.ts
@@ -2,11 +2,9 @@
 //@author James Church
 
 import readlineSync = require('readline-sync'); //for easier repeated prompts
-import {Product} from './products';
-
-// Hey look. It's a global variable. This is totally cool, right?
-let shopping_cart: Product[] = [];
-let quantity_cart: number[] = [];
+import { Product } from './products';
+import { ShopModel } from './shopModel';
+import { ProductView } from './productView';
 
 /**
  * Function to run the UI
@@ -49,6 +47,8 @@ function addItemToCart() {
 }
 
 function letUserSelectItem() {
+    let shopModel = new ShopModel;
+
     console.log(`Here you can select your shape. Pick an option:
   1. Buy a Triangle!
   2. Buy a Square!
@@ -57,55 +57,51 @@ function letUserSelectItem() {
 
     let response = readlineSync.question('> ')
 
-    switch(response) { //handle each response
-      case '1': shopping_cart.push(new Product("Triangle", 3.5, "It's got three sides!")); break;
-      case '2': shopping_cart.push(new Product("Square", 4.5, "It's got four sides!")); break;
-      case '3': shopping_cart.push(new Product("Pentagon", 5.5, "It's got five sides!")); break;
-      default: console.log('Invalid option!');
+    switch (response) { //handle each response
+        case '1': shopModel.addProduct("Triangle", 3.5, "It's got three sides!"); break;
+        case '2': shopModel.addProduct("Square", 4.5, "It's got four sides!"); break;
+        case '3': shopModel.addProduct("Pentagon", 5.5, "It's got five sides!"); break;
+        default: console.log('Invalid option!');
     }
     console.log(''); //extra empty line for revisiting
 }
 
 function letUserSelectQuantity() {
+    let shopModel = new ShopModel;
     console.log(`How many of this shape would you like to purchase?
   `);
 
     let response = readlineSync.question('> ')
-    quantity_cart.push(parseInt(response));
+    shopModel.addQuantity(parseInt(response));
     console.log(''); //extra empty line for revisiting
 }
 
 function removeItemFromCart() {
-    console.log(`Select an item to be removed from the cart.
-  `);
+    let shopModel = new ShopModel;
+    let products = new ProductView(shopModel);
 
-    for (let i = 0; i < shopping_cart.length; i++) {
-        console.log(i+": "+shopping_cart[i].getName());
-    }
+    console.log(`Select an item to be removed from the cart.`);
+
+    console.log(products.getNameView());
 
     let response = readlineSync.question('> ')
-    let toRemove = parseInt(response);
+    let removeMe = parseInt(response);
 
-    shopping_cart.splice(toRemove, 1);
-    quantity_cart.splice(toRemove, 1);
+    shopModel.removeProduct(removeMe);
 
     console.log(''); //extra empty line for revisiting
 }
 
 function viewItemsInCart() {
-    for (let i = 0; i < shopping_cart.length; i++) {
-        console.log("");
-        console.log("       Name: "+shopping_cart[i].getName());
-        console.log("      Price: "+shopping_cart[i].getPrice());
-        console.log("Description: "+shopping_cart[i].getDescription());
-        console.log("   Quantity: "+quantity_cart[i]);
-    }
+    let shopModel = new ShopModel;
+    let products = new ProductView(shopModel);
+
+    console.log(products.getView)
 }
 
 function viewCartTotal() {
-    let total: number = 0;
-    for (let i = 0; i < shopping_cart.length; i++) {
-        total += shopping_cart[i].getPrice() * quantity_cart[i];
-    }
-    console.log("Shopping Cart Total: "+total);
+    let shopModel = new ShopModel;
+    let products = new ProductView(shopModel);
+
+    console.log(products.getPriceView);
 }
